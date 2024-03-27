f8b55fc44d6864b74aa7048504a3bc9d8f7fafe3
Odarell35 <leratomawash0@gmail.com>
2024-03-27 15:52:37 +0000

    tasks and readme

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Writing strings to Redis"""
+
+import uuid
+import redis
+from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+        """docs"""
+        @wraps(method)
+        def wrapper(*args, **kwargs):
+            """doc"""
+            wrapper.call_count += 1
+            return method(*args, **kwargs)
+
+        wrapper.call_count = 0
+        wrapper.__name__ = f"counted_{method.__qualname__}"
+        return wrapper
+
+class Cache():
+    """create class cache"""
+
+    def __init__(self) -> None:
+        """init method"""
+        self._redis = redis.Redis(host='localhost', port=6379, db=0)
+        self._redis.flushdb()
+
+    def store(self, data:Union[str, bytes, int, float]) -> str:
+        """generate a random key """
+        ran_key = str(uuid.uuid4())
+        self._redis.set(ran_key, data)
+
+        return ran_key
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], any]] = None) -> any:
+        """create a get method that take a key string argument"""
+        data = self._redis.get(key)
+
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """docs"""
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[str]:
+        """docs"""
+        return self.get(key, fn=int)
